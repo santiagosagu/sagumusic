eb0c2e84d0d7db7a30cc20d50c2e34362a4a84d7
daniel.giraldo.atlanticsoft <daniel.giraldo.atlanticsoft@gmail.com>
2024-08-03 08:50:11 -0500

    correction pwa

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,11 @@ async function enableMocking() {
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: "/mockServiceWorker.js", // Asegúrate de que esta ruta es accesible
+    },
+  });
 }
 
 enableMocking().then(() => {
