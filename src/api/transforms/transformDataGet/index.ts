f55e91f0fe42d10a2b53a2c206e6d4ecf05f66e6
daniel.giraldo.atlanticsoft <daniel.giraldo.atlanticsoft@gmail.com>
2024-08-05 21:26:49 -0500

    remove comment code

--- src/api/transforms/transformDataGet/index.ts
+++ src/api/transforms/transformDataGet/index.ts
@@ -2,12 +2,6 @@
 
 const dataTransform = (data: any, keyResults: string) => {
   return {
-    // metaData: {
-    //   records_count: data.records_count,
-    //   limit: data.limit,
-    //   next_page: data.next_page,
-    //   previous_page: data.previous_page,
-    // },
     metaData: data.info,
     results: data[keyResults],
   };
